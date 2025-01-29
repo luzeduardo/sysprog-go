55201c01ae6f7b3c83e883a59a82ef6a14b758eb
Eduardo Luz <luz.eduardo@gmail.com>
2025-01-29 12:15:18 +0000

    uses mutexes to avoid race conditions

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	mainC1P43Mutexes()
+
+}
+
+func mainC1P43Mutexes() {
+	m := sync.Mutex{}
+	fmt.Println("Total Items Packed: ", PackItems(&m, 0))
+}
+
+func PackItems(m *sync.Mutex, totalItems int) int {
+	const workers = 2
+	const itemsPerWorker = 1000
+
+	var wg sync.WaitGroup
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			for j := 0; j < itemsPerWorker; j++ {
+				m.Lock()
+				itemsPacked := totalItems
+				itemsPacked++
+				totalItems = itemsPacked
+				m.Unlock()
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	return totalItems
+}
