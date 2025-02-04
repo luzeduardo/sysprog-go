93969f8c8ba9ad3778ed0fbd79c7b69491ffdec7
Eduardo Luz <luz.eduardo@gmail.com>
2025-02-04 12:46:47 +0000

    C1P35 - signaling between gotourines

--- main.go
+++ main.go
@@ -3,16 +3,83 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
-	mainC1ChannelWaitGroup()
+	mainC1P35SignalingChannel()
+	// mainC1P30BufferedChannels()
+	// mainC1ChannelWaitGroup()
 	// mainC1Channel()
-	// mainC1P43Mutexes()
+	// mainC1P22Mutexes()
+}
+
+func mainC1P35SignalingChannel() {
+	var wg sync.WaitGroup
+	signalChannel := make(chan bool)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Goroutine 1: Waiting for signal")
+		<-signalChannel
+		fmt.Println("Goroutine 1: Received signal")
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Goroutine 2 is about to send a signal")
+		signalChannel <- true
+		fmt.Println("Goroutine 2: Sent signal")
+	}()
+
+	wg.Wait()
+	fmt.Println("All goroutines have finished their jobs")
+}
+
+func mainC1P30BufferedChannels() {
+	clownChannel := make(chan int, 3)
+	clowns := 5
+
+	go func() {
+		defer close(clownChannel)
+		for clownID := range clownChannel {
+			ballon := fmt.Sprintf("Ballon %d", clownID)
+			fmt.Printf("Driver: Drove the car with %s inside \n", ballon)
+
+			time.Sleep(time.Millisecond * 500)
+			fmt.Printf("Driver: Clown finished with %s, the car is ready for more!\n", ballon)
+		}
+	}()
+
+	var wg sync.WaitGroup
+
+	for clown := 1; clown <= clowns; clown++ {
+		wg.Add(1)
+
+		go func(clownID int) {
+			defer wg.Done()
+			ballon := fmt.Sprintf("Ballon %d", clownID)
+			fmt.Printf("Clown %d: Hopped into the car with %s\n", clownID, ballon)
+			select {
+			case clownChannel <- clownID:
+				fmt.Printf("Clown %d: Finished with %s\n", clownID, ballon)
+			default:
+				fmt.Printf("Clown %d: Ops the car is full, can't fit %s!\n", clownID, ballon)
+			}
+		}(clown)
+
+		fmt.Println("Circus car ride is over")
+	}
+
+	wg.Wait()
+	fmt.Println("Circus car ride is over")
 }
 
 func mainC1ChannelWaitGroup() {
 	balls := make(chan string)
+	// create a WaitGroup to wait for the goroutines to finish
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 
@@ -29,6 +96,7 @@ func mainC1ChannelWaitGroup() {
 	go func() {
 		wg.Wait()
 		// close the channel after the goroutines are done
+		// so the range loop in the main goroutine can finish
 		close(balls)
 	}()
 
@@ -48,7 +116,7 @@ func throwBalls(color string, balls chan string) {
 	balls <- color
 }
 
-func mainC1P43Mutexes() {
+func mainC1P22Mutexes() {
 	m := sync.Mutex{}
 	fmt.Println("Total Items Packed: ", PackItems(&m, 0))
 }
